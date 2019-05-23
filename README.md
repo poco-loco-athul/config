93b2c62d14fe6b49c2a80460cbda63f91cfd0060
Athul KP <kpathul@outlook.com>
2019-05-24 00:53:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# config
+A collection of personal config files for different applications.
---
+++ link_config.sh
@@ -0,0 +1,3 @@
+ln -s ~/config/bash_aliases ~/.bash_aliases
+ln -s ~/config/.emacs ~/.emacs
+ln -s ~/config/gitconfig ~/.gitconfig
