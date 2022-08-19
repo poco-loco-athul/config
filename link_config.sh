ec1dc78096d744c069fac8c80acfdfa496b6a095
Athul KP <kpathul@outlook.com>
2022-08-19 15:27:52 +0530

    Adds custom bash commands.

--- link_config.sh
+++ link_config.sh
@@ -1,11 +1,9 @@
-ln ~/config/bash_aliases ~/.bash_aliases
 ln ~/config/emacs ~/.emacs
 ln ~/config/gitconfig ~/.gitconfig
 ln ~/config/pythonrc ~/.pythonrc
 
-echo "export GIT_PS1_SHOWDIRTYSTATE=1" >> ~/.bashrc
-echo "export GIT_PS1_SHOWCOLORHINTS=1" >> ~/.bashrc
-echo "export GIT_PS1_SHOWUNTRACKEDFILES=1" >> ~/.bashrc
-echo "export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '" >> ~/.bashrc
 
-echo ". ~/config/z.sh" >> ~/.bashrc
+echo "# Custom changes follows:
+if [ -f ~/config/bash_custom ]; then
+    . ~/config/bash_custom
+fi" >> ~/.bashrc
